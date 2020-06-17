cea933841a2915169fc3c881aa756b697bd95f6c
noctisilva <taeiscoolaboy@gmail.com>
2020-06-16 21:38:39 -0700

    intial commit

---
+++ tableview_swift_practice/tableview_swift_practice/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tableview_swift_practice
+//
+//  Created by Tae Hong Min on 6/16/20.
+//  Copyright © 2020 tableview.practice.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
